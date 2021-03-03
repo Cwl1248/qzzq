85679617867c01240bd2b73aa08e0179667e5fd0
beason353739 <2368702029@qq.cpm>
2021-03-03 19:29:59 +0800

    first commit

---
+++ src/main/java/com/cwl/qzzp/QzzpApplication.java
@@ -0,0 +1,13 @@
+package com.cwl.qzzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzzpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QzzpApplication.class, args);
+    }
+
+}
