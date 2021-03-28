6f6c1d1203c8bc09c617d9ed9b9a834fa84a2deb
beason353739 <2368702029@qq.cpm>
2021-03-28 23:06:47 +0800

    更新内容：代码格式修改 更新时间：2021年3月28日23:06:08

--- src/main/java/com/cwl/qzzp/controllor/PositionController.java
+++ src/main/java/com/cwl/qzzp/controllor/PositionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Slf4j
 @Controller
-public class positionController {
+public class PositionController {
 
     @GetMapping("/positionlist")
     public String getPositionData() {
