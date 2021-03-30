15ecc2af0eafa7fe71b3b2642ed6aa6afba06356
beason353739 <2368702029@qq.cpm>
2021-03-30 08:49:13 +0800

    更新内容：公司信息列表 更新时间：2021年3月30日08:48:50

---
+++ src/main/java/com/cwl/qzzp/controllor/CompanyListController.java
@@ -0,0 +1,21 @@
+package com.cwl.qzzp.controllor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author ：ChengWl
+ * @date ：Created in 2021/3/29
+ * @description：招聘公司
+ */
+@Slf4j
+@Controller
+public class CompanyListController {
+
+    @GetMapping("/companyList")
+    public String getCompanyListData() {
+
+        return "companyList/companyList";
+    }
+}
