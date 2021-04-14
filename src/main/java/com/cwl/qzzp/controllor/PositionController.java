87f29c3ca25da412011500c61487132c9634d006
beason353739 <2368702029@qq.cpm>
2021-04-14 22:52:58 +0800

    更新内容：样式小修改，head.html 添加了后端链接 更新时间：2021年4月14日22:52:50

--- src/main/java/com/cwl/qzzp/controllor/PositionController.java
+++ src/main/java/com/cwl/qzzp/controllor/PositionController.java
@@ -3,6 +3,7 @@ package com.cwl.qzzp.controllor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author ：ChengWl
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Slf4j
 @Controller
+@RequestMapping("/position")
 public class PositionController {
 
     @GetMapping("/positionList")
